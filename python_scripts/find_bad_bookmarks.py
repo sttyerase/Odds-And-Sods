69182f24685a7a0f5f2e1c40ce13a871fcdaf77b
Dan Moore <sttyerase@gmail.com>
2019-11-25 09:17:24 -0500

    Updates and fixes.  Add DEBUG mode.

--- python_scripts/find_bad_bookmarks.py
+++ python_scripts/find_bad_bookmarks.py
@@ -1,31 +1,39 @@
-
-## PYTHON PROGRAM TO LOOP A LIST OF BOOKMARKS AND
+#! /usr/bin/python
+## PYTHON PROGRAM TO LOOP A FILE OF BOOKMARKS EXPORTED FROM CHROME AND
 ## REPORT ERRORS
+## TO RUN IN DEBUG MODE SET ENVIRONMENT VARIABLE DEBUG=true
 
 from urllib2 import Request, urlopen, URLError, HTTPError
-import fileinput
-fname = './bookmarks.html'
+import fileinput, os
 
 ### OPEN THE FILE AND READ EACH LINE
+count = 0
+DEBUG=os.environ['DEBUG']
+if(DEBUG == 'true'): print 'DEBUG: ', DEBUG
 for line in fileinput.input():
-    if(line.find('<A') > 0):
-        strstart  =  line.find('<A')
+    count += 1
+    if(line.find('HREF=') > 0):
+        strstart  =  line.find('HREF=')
         strend    =  line.find('>',strstart + 1)
-        ## print ': ' + str(strstart) + ' : ' + str(strend)
         subline   = line[strstart:strend+1]
         urlstart  = subline.find('"')
         urlend    = subline.find('"',urlstart+1)
         urlstr    = subline[urlstart+1:urlend]
+        user_agent= 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1)'
+        headers = {'User-Agent' : user_agent}
         req       = Request(urlstr)
+        req.add_header('User-Agent',user_agent)
+        if(DEBUG == 'true'): print '####: ', urlstr,' METH: ', req.get_method(),' $$$$: ',req.has_data()
         ### VALIDATE EACH URL
         try:
-            response = urlopen(req)
+            response = urlopen(req, timeout = 10)
         except HTTPError as e:
-            print urlstr
+            print count, ' : ' ,urlstr
             print 'The server couldn\'t fulfill the request.'
-            print 'Error code: ', e.code
+            print 'Error code: ', e.code, ' : ', e.reason
         except URLError as e:
-            print urlstr
+            print count, ' : ' ,urlstr
             print 'We failed to reach a server.'
             print 'Reason: ', e.reason
-
+    if(DEBUG == 'true'): print count
+print 'Operation Complete.'
