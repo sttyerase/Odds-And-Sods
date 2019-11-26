4b0fdd65a83be1b87a2cce50bcc3a8c4137bf7cc
Dan Moore <sttyerase@gmail.com>
2019-11-25 23:03:02 -0500

    DEBUG environment variable handling.  Added comments.

--- python_scripts/find_bad_bookmarks.py
+++ python_scripts/find_bad_bookmarks.py
@@ -2,13 +2,21 @@
 ## PYTHON PROGRAM TO LOOP A FILE OF BOOKMARKS EXPORTED FROM CHROME AND
 ## REPORT ERRORS
 ## TO RUN IN DEBUG MODE SET ENVIRONMENT VARIABLE DEBUG=true
+## e.g., at the prompt enter:
+##    DEBUG=true find_bad_bookmarks.py <bookmarks filename>
 
 from urllib2 import Request, urlopen, URLError, HTTPError
 import fileinput, os
 
 ### OPEN THE FILE AND READ EACH LINE
 count = 0
-DEBUG=os.environ['DEBUG']
+try:
+    DEBUG=os.environ['DEBUG']
+except AttributeError:
+    DEBUG = 'false'
+except KeyError:
+    DEBUG = 'false'
+## BEGIN PROCESSING
 if(DEBUG == 'true'): print 'DEBUG: ', DEBUG
 for line in fileinput.input():
     count += 1
@@ -20,7 +28,7 @@ for line in fileinput.input():
         urlend    = subline.find('"',urlstart+1)
         urlstr    = subline[urlstart+1:urlend]
         user_agent= 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1)'
-        headers = {'User-Agent' : user_agent}
+        ## FORCE GET METHOD.  ADD USER-AGENT HEADER TO REQUEST
         req       = Request(urlstr)
         req.add_header('User-Agent',user_agent)
         if(DEBUG == 'true'): print '####: ', urlstr,' METH: ', req.get_method(),' $$$$: ',req.has_data()
