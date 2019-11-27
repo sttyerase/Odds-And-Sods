6f1d2792cf141054ce7f1b311c8079d56f13444c
Dan Moore <sttyerase@gmail.com>
2019-11-27 10:22:00 -0500

    Simplify bookmark parsing. Add comments. Add SSLError trapping.

--- python_scripts/find_bad_bookmarks.py
+++ python_scripts/find_bad_bookmarks.py
@@ -6,27 +6,32 @@
 ##    DEBUG=true find_bad_bookmarks.py <bookmarks filename>
 
 from urllib2 import Request, urlopen, URLError, HTTPError
-import fileinput, os
+import fileinput, os, ssl
+from pip._vendor.urllib3.exceptions import SSLError
 
 ### OPEN THE FILE AND READ EACH LINE
 count = 0
+## DETERMINE DEBUG MODE AND OTHER ENV SETTINGS
 try:
     DEBUG=os.environ['DEBUG']
 except AttributeError:
     DEBUG = 'false'
 except KeyError:
     DEBUG = 'false'
-## BEGIN PROCESSING
 if(DEBUG == 'true'): print 'DEBUG: ', DEBUG
+## BEGIN PROCESSING
+## READ EACH LINE AND PARSE FOR WEB URL USING HREF=
 for line in fileinput.input():
     count += 1
     if(line.find('HREF=') > 0):
-        strstart  =  line.find('HREF=')
-        strend    =  line.find('>',strstart + 1)
-        subline   = line[strstart:strend+1]
-        urlstart  = subline.find('"')
-        urlend    = subline.find('"',urlstart+1)
-        urlstr    = subline[urlstart+1:urlend]
+        try:
+            strstart  =  line.index('HREF=')
+            urlstart  =  line.index('"',strstart + 1)
+            urlend    =  line.index('"',urlstart + 1)
+            urlstr    =  line[urlstart+1:urlend]
+        except ValueError as ve:
+            print 'ValueError: ', ve
+            continue
         user_agent= 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1)'
         ## FORCE GET METHOD.  ADD USER-AGENT HEADER TO REQUEST
         req       = Request(urlstr)
@@ -43,5 +48,8 @@ for line in fileinput.input():
             print count, ' : ' ,urlstr
             print 'We failed to reach a server.'
             print 'Reason: ', e.reason
+        except ssl.SSLError as ssle:
+            print count, ' : ' ,urlstr
+            print 'SSL Error: ', ssle
     if(DEBUG == 'true'): print count
 print 'Operation Complete.'
